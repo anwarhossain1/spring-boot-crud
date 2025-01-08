102e04db3524bbc956a90b8b6e1233f4c443dda2
anwarhossain1 <irishkhan33@gmail.com>
2025-01-08 13:01:27 +0600

    feat: patient entity added

---
+++ src/main/java/com/example/spring_boot/entity/Patient.java
@@ -0,0 +1,17 @@
+package com.example.spring_boot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//this entity as orm for mongodb
+@Data
+@AllArgsConstructor
+@Document(collection = "patients")
+public class Patient {
+    private String id;
+    private String name;
+    private String email;
+    private int age;
+}
+
